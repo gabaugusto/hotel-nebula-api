a841d08ac6f68aefbaf416181c27df95a1756306
Gabriel Augusto <33496735+gabaugusto@users.noreply.github.com>
2026-03-31 11:28:58 -0300

    Create NebulahotelApplication.kt

---
+++ api-kotlin-nosql/src/main/kotlin/com/nebulahotel/NebulahotelApplication.kt
@@ -0,0 +1,11 @@
+package com.nebulahotel
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NebulahotelApplication
+
+fun main(args: Array<String>) {
+	runApplication<NebulahotelApplication>(*args)
+}
